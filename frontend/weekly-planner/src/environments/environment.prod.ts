6ec77e4f6d5263264b5bd9b134cb84e10ff76ba6
Gaurav Prajapati <gauravprajapati9370@gmail.com>
2026-03-05 12:57:44 +0530

    fix: change frontend apiUrl to allow cors

--- frontend/weekly-planner/src/environments/environment.prod.ts
+++ frontend/weekly-planner/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://weekly-planner-project-api.azurewebsites.net/api'
+    apiUrl: 'https://weekly-planner-project-api.azurewebsites.net/'
     // apiUrl: 'http://localhost:5244/api'
 };
--- frontend/weekly-planner/src/environments/environment.ts
+++ frontend/weekly-planner/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // apiUrl: 'http://localhost:5244/api'
-    apiUrl: 'https://weekly-planner-project-api.azurewebsites.net/api'
+    apiUrl: 'https://weekly-planner-project-api.azurewebsites.net/'
 };
