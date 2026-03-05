f047fb9a3b2c533acd9a5431cf708d5762606a5a
Gaurav Prajapati <gauravprajapati9370@gmail.com>
2026-03-05 12:33:33 +0530

    ci: add GitHub Actions CI/CD for backend and frontend Azure deployment

--- frontend/weekly-planner/src/environments/environment.prod.ts
+++ frontend/weekly-planner/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://weekly-planner-api.azurewebsites.net/api'
-    apiUrl: 'http://localhost:5244/api'
+    apiUrl: 'https://weekly-planner-project-api.azurewebsites.net/'
+    // apiUrl: 'http://localhost:5244/api'
 };
--- frontend/weekly-planner/src/environments/environment.ts
+++ frontend/weekly-planner/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5244/api'
+    // apiUrl: 'http://localhost:5244/api'
+    apiUrl: 'https://weekly-planner-project-api.azurewebsites.net/'
 };
