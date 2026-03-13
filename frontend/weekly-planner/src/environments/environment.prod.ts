63d8b5b5b6a373f21a69999a1ada687872dc3e78
Gaurav Prajapati <gauravprajapati9370@gmail.com>
2026-03-13 18:39:18 +0530

    fix(config): update API URL to new Azure App Service endpoint

--- frontend/weekly-planner/src/environments/environment.prod.ts
+++ frontend/weekly-planner/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://weekly-planner-project-api.azurewebsites.net/api'
+    apiUrl: 'https://weekly-planner-project-apis.azurewebsites.net/api'
     // apiUrl: 'http://localhost:5244/api'
 };
